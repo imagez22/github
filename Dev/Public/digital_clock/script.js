55635edbdceade5152b3df920e0e6ca5638a107d
imagez22 <aklijoshuaericy@gmail.com>
2022-03-29 14:01:54 +0000

    desktop changes..now commiting to github...safe changes...checked

--- Dev/Public/digital_clock/script.js
+++ Dev/Public/digital_clock/script.js
@@ -1,23 +1,23 @@
-function showTime(){
-  var date= new Date();
-  var h=date.getHours();
-  var m= date.getMinutes();
-  var s= date.getSeconds();
-  var session="AM";
-  if(h==0){
-    h= 12;
+function showTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  var session = "AM";
+  if (hour == 0) {
+    hour = 12;
   }
-  if (h>12){
-    h=h-12
-    session="PM";
+  if (hour > 12) {
+    hour = hour - 12;
+    session = "PM";
   }
-  h=(h<10) ? "0" + h:h;
-  m=(m<10) ? "0" + m:m;
-  s=(s<10) ? "0" + s:s;
-  var time = h + ":" + m +":" + s + " " + session;
+  hour = (hour < 10) ? "0" + hour : hour;
+  minute = (minute < 10) ? "0" + minute : minute;
+  second = (second < 10) ? "0" + second : second;
+  var time = hour + ":" + minute + ":" + second + " " + session;
   document.getElementById("clockDisplay").innerText = time;
   document.getElementById("clockDisplay").textContent = time;
   document.getElementById("clockDisplay").innerHTML = time;
-  setTimeout(showTime,1000);
+  setTimeout(showTime, 1000);
 }
 showTime();
